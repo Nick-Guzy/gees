dbc64a626726fa84b6503ee5db8f36c33d486332
Nick-Guzy <nick.guzy76@gmail.com>
2023-05-05 12:02:08 -0700

    Footer placed at bottom of page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,22 @@ import React from "react";
 
 function Footer() {
   return(
-    <><p><b>CONTACT US: | Los Angeles, CA:</b>1-213-453-7898<b>Email:</b>info@GeesCalmBalm.com</p></>
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "left",
+      bottom: 0,
+      width: "100%",
+      height: "5vh",
+      position: "fixed",
+      backgroundColor: "lightgray",
+      
+    }}>
+    <><p><b>CONTACT US: | Los Angeles, CA:</b>1-213-453-7898<b> Email:</b>info@GeesCalmBalm.com</p></>
+    </div>
   );
 }
 
 export default Footer;
+
+
