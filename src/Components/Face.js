3eda345f61ec9f68abdedbf7d3bee33275455507
Nick-Guzy <nick.guzy76@gmail.com>
2023-05-10 14:45:03 -0700

    Face and Serum product pages built. Need to adjust styling to see bottom most product

--- src/Components/Face.js
+++ src/Components/Face.js
@@ -1,12 +1,76 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Cream2 from "./../pics/Cream2.jpg";
+import Cream3 from "./../pics/Cream3.jpg";
+import Cream4 from "./../pics/Cream4.jpg";
 
 function Face() {
-  return(
-    <div> 
+  return (
+    <>
+    <div>   
       <h1>Face</h1>
     </div>
-  
-  
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={Cream2} style={{ height: '300px' }} />
+      <Card.Body>
+        <Card.Title>Rejuvinating Morning Citrus</Card.Title>
+        <Card.Text>
+         This lightweight cream wil leave you feeling hydrated and energized!
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Price: $20</ListGroup.Item>
+        <ListGroup.Item>Size: 10oz</ListGroup.Item>
+        <ListGroup.Item>Ingridients: Orange and grapefruit extract</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Add to cart/Details/Sizes</Card.Link>
+        {/* <Card.Link href="#">Details/Sizes</Card.Link> */}
+      </Card.Body>
+    </Card>
+    
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Cream3}/>
+      <Card.Body>
+        <Card.Title>Calming Hydration Balm</Card.Title>
+        <Card.Text>
+         This balm is a rich extra moisturizing balm for extra dry skin.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Price: $20</ListGroup.Item>
+        <ListGroup.Item>Size: 10oz</ListGroup.Item>
+        <ListGroup.Item>Ingridients: Shea butter</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Add to cart/Details/Sizes</Card.Link>
+        {/* <Card.Link href="#">Details/Sizes</Card.Link> */}
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Cream4} />
+      <Card.Body>
+        <Card.Title>Calming Hydration Balm</Card.Title>
+        <Card.Text>
+         This balm is a rich extra moisturizing balm for extra dry skin.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Price: $20</ListGroup.Item>
+        <ListGroup.Item>Size: 10oz</ListGroup.Item>
+        <ListGroup.Item>Ingridients: Shea butter</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Add to cart/Details/Sizes</Card.Link>
+        {/* <Card.Link href="#">Details/Sizes</Card.Link> */}
+      </Card.Body>
+    </Card>
+    </div>
+    </>
+    
   );
 }
 
